8065881f4793e2b20734849b651ba5e6f05ffd12
Thomas Lacombe <tomtom637@gmail.com>
2022-07-24 14:35:42 +0200

    added active links

--- src/components/MainNav/MainNav.jsx
+++ src/components/MainNav/MainNav.jsx
@@ -1,17 +1,25 @@
 import { NavLink } from "react-router-dom";
 import style from "./MainNav.module.scss";
 
+const activeStyle = {
+  borderBottom: "2px solid var(--pink)",
+};
+
 export default function () {
   return (
-      <nav className={style['main-nav']}>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
-          </li>
-        </ul>
-      </nav>
+    <nav className={style['main-nav']}>
+      <ul>
+        <li>
+          <NavLink
+            style={({ isActive }) => isActive ? activeStyle : undefined}
+            to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink
+            style={({ isActive }) => isActive ? activeStyle : undefined}
+            to="/about">About</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
